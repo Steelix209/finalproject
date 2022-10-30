d0c5aa91eabc4dba47c5b0ebc183b60990d68297
ZentosBG <113288639+ZentosBG@users.noreply.github.com>
2022-10-30 14:58:38 +0200

    U
    YYIU

--- maim.py
+++ maim.py
@@ -26,7 +26,7 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def __init__(self):
-        super().__init__("dino.png", 200, 200, 75, 75)
+        super().__init__("dino.png", 200, ground - 75, 75, 75)
         self.speed = 5
         self.hp = 100
 
@@ -45,19 +45,27 @@ class Player(GameSprite):
 
 class Cactus(GameSprite):
     def __init__(self, x, y, width, height, color = (255, 113, 13)):
-        super().__init__("cactus2.png", x, y, width, height)
-        self.rect.x = x
-        self.rect.y = y
+        super().__init__("cactus2.png", x, ground - height, width, height)
         self.width = width
         self.height = height
+    def update (self):
+        self.rect.x -= bg_speed
+        if self.rect.x >- 0:
+            self.kill()
 run = True
 finish = False
-
+ground = HEIGHT - 60
+bg1x = 0
+bg2x = WIDTH
 
 
 bg_image  = transform.scale(image.load("2.jpg"), (WIDTH, HEIGHT))
+bg_image2 = transform.scale(image.load("2.jpg"), (WIDTH, HEIGHT))
+bg_speed = 2
 player = Player()
-cactus = Cactus(100, 100, 50, 50)
+cactus = Cactus(100, 500, 100, 100)
+cactuses = sprite.Group()
+cactuses.add(cactus)
 run = True
 game = False
 FPS = 60
@@ -67,8 +75,18 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+    bg1x = bg1x - bg_speed
+    bg2x = bg2x - bg_speed
+    if bg1x <-WIDTH:
+        bg1x = WIDTH
+    if bg2x <-WIDTH:
+        bg2x = WIDTH
+    
+    cactuses.update()
+    window.blit(bg_image, (bg1x,0))
+    window.blit(bg_image2, (bg2x,0))
 
-    window.blit(bg_image, (0,0))
+    
     player.draw()
     cactus.draw()
     
